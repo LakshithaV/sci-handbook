956eaef523b424ba801a328836779890a3eca4fc
LakshithaV <lakshithavirajkarunarathna@gmail.com>
2025-10-27 11:00:14 +0530

    Folder Structure

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch README.md
+touch requirements.txt
+touch research/trails.ipynb
+
+echo "Created project structure with necessary files."
