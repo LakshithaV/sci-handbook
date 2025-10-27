f51013325ca7d2f474dc9690d2069714dd6d0957
LakshithaV <lakshithavirajkarunarathna@gmail.com>
2025-10-27 22:53:19 +0530

    app complete

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sci-handbook',
+    version='0.1.0',
+    author='Lakshitha Viraj Karunarathna',
+    author_email='lakshitha.v@dcs.ruh.ac.lk',
+    packages=find_packages(),
+    install_requires=[]
+)
